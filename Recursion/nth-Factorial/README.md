cb3fdcc62de1004cc45e1b3839891b7f8509bf54
frankymacster <fs.sunatori@me.com>
2018-10-12 15:13:32 -0400

    add Recursion section and nth-Factorial algorithm

---
+++ Recursion/nth-Factorial/README.md
@@ -0,0 +1,9 @@
+# Fibonacci Sequence
+Finding Fibonacci sequence using dynamic programming.
+
+## Complexity
+* **Time**: ![](https://latex.codecogs.com/svg.latex?O(n))
+* **Space**: ![](https://latex.codecogs.com/svg.latex?O(n))
+
+## References
+* [Wikipedia](https://en.wikipedia.org/wiki/Dynamic_programming#Fibonacci_sequence)
---
+++ Recursion/nth-Factorial/code.js
@@ -0,0 +1,27 @@
+import { Array1DTracer } from 'algorithm-visualizer';
+
+var tracer = new Array1DTracer('Sequence');
+var index = 15;
+var D = [1];
+for (var i = 1; i < index; i++) {
+    D.push(0);
+}
+tracer._setData(D);
+
+function fact(num) {
+  if (num === 1) {
+      return 1;
+  }
+
+  var res = num * fact(num - 1);
+
+  D[num - 1] = res;
+  
+  tracer._select(num - 1)._wait();
+  tracer._notify(num - 1, D[num - 1])._wait();
+  tracer._denotify(num - 1);
+  tracer._deselect(num - 1);
+
+  return res;
+}
+fact(index);
